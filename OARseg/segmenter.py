8051b8ef25afb14077125ffef1ab78f005c92a44
mp31192 <120339676@qq.com>
2020-06-09 14:46:55 +0800

    change

--- OARseg/segmenter.py
+++ OARseg/segmenter.py
@@ -12,6 +12,7 @@ import systemsetup
 import xlwt, xlrd
 import xlutils.copy
 import copy
+import random
 from skimage.measure import regionprops
 from torch.utils.checkpoint import checkpoint
 
@@ -68,10 +69,12 @@ class Segmenter:
         self.checkpointsBasePathSave= systemsetup.CHECKPOINT_BASE_PATH
         self.predictionsBasePath = systemsetup.PREDICTIONS_BASE_PATH
         self.startFromEpoch = 1
+        self.mode = "train"
 
         self.bestMeanDice = 0
         self.bestMeanDiceEpoch = 0
 
+        self.epoch_now = 0
         self.movingAvg = 0
         self.bestMovingAvg = 0
         self.bestMovingAvgEpoch = 1e9
@@ -131,6 +134,7 @@ class Segmenter:
             os.makedirs(basePath)
 
         expConfig.net.eval()
+        self.mode = "eval"
 
         with torch.no_grad():
             for i, data in enumerate(self.testDataLoader):
@@ -252,6 +256,7 @@ class Segmenter:
 
             # set net up training
             self.expConfig.net.train()
+            self.mode = "train"
 
             for i, data in enumerate(self.trainDataLoader):
 
@@ -449,8 +454,10 @@ class Segmenter:
         print('======================================')
 
         epoch = self.startFromEpoch
+        self.epoch_now = epoch
         while epoch < expConfig.EPOCHS and epoch <= self.bestMovingAvgEpoch + self.EARLY_STOPPING_AFTER_EPOCHS:
             expConfig.net.train()
+            self.mode = "train"
 
             expConfig.net = expConfig.net.to(self.device)
             expConfig.optimizer.zero_grad()
@@ -463,10 +470,10 @@ class Segmenter:
 
                 # load data
                 inputs_list, pid, labels_list = data
-                for ini in range(len(inputs_list)):
-                    inputs_list[ini] = inputs_list[ini].to(self.device)
-                for lai in range(len(labels_list)):
-                    labels_list[lai] = labels_list[lai].to(self.device)
+                # for ini in range(len(inputs_list)):
+                #     inputs_list[ini] = inputs_list[ini].to(self.device)
+                # for lai in range(len(labels_list)):
+                #     labels_list[lai] = labels_list[lai].to(self.device)
 
                 ori_inputs, ori_labels = inputs_list[0], labels_list[0]
                 ori_local_result = inputs_list[1]
@@ -488,12 +495,11 @@ class Segmenter:
                                 outputs_mrf = torch.zeros_like(local_result)
                                 organs_size_flag = organs_size
                             else:
-                                outputs_mrf = torch.from_numpy(outputs_final_seg.detach().cpu().numpy()).to(self.device)
+                                outputs_mrf = torch.from_numpy(outputs_final_seg.detach().cpu().numpy())
                                 organs_size_flag = organs_size
                         else:
-                            outputs_deform_new = torch.from_numpy(outputs_final_seg.detach().cpu().numpy()).to(
-                                self.device)
-                            outputs_mrf = torch.from_numpy(outputs_final_seg.detach().cpu().numpy()).to(self.device)
+                            outputs_deform_new = torch.from_numpy(outputs_final_seg.detach().cpu().numpy())
+                            outputs_mrf = torch.from_numpy(outputs_final_seg.detach().cpu().numpy())
                             if mrf_i == 0:
                                 organs_size_flag = organs_size
                             else:
@@ -513,7 +519,7 @@ class Segmenter:
                             organ_name = organ_name_LR
 
                         class_id = organs_size_class[organ_name]
-                        labels_class = torch.tensor([class_id]).to(self.device)
+                        labels_class = torch.tensor([class_id])
                         crop_size = organs_size_flag[organ_name]
                         organ_id_combine = organs_combine[organ_name_LR]
 
@@ -662,6 +668,7 @@ class Segmenter:
                 self.saveToDisk(epoch)
 
             epoch = epoch + 1
+            self.epoch_now = epoch
 
         #print best mean dice
         print("ID:",expConfig.id)
@@ -673,6 +680,7 @@ class Segmenter:
         expConfig = self.expConfig
         expConfig.net.eval()
 
+        self.mode = "eval"
         startTime = time.time()
         with torch.no_grad():
             diceWT = []
@@ -834,7 +842,6 @@ class Segmenter:
             print(additionalString + "Memory {:.0f}Mb max, {:.0f}Mb current".format(
                 torch.cuda.max_memory_allocated() / 1024 / 1024, torch.cuda.memory_allocated() / 1024 / 1024))
 
-
     def saveToDisk(self, epoch):
 
         #gather things to save
@@ -866,7 +873,7 @@ class Segmenter:
         self.expConfig.net.load_state_dict(checkpoint["net_state_dict"])
 
         #load optimizer: hack necessary because load_state_dict has bugs (See https://github.com/pytorch/pytorch/issues/2830#issuecomment-336194949)
-        self.expConfig.optimizer.load_state_dict(checkpoint["optimizer_state_dict_seg"])
+        self.expConfig.optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
         for state in self.expConfig.optimizer.state.values():
             for k, v in state.items():
                 if isinstance(v, torch.Tensor):
@@ -911,59 +918,119 @@ class Segmenter:
             self.bestMovingAvg = self.movingAvg
             self.bestMovingAvgEpoch = epoch
 
-    def new_crop(self, outputs):
-        outputs_cpu = outputs.detach().cpu().numpy()
-        # b, c, x, y, z = outputs_cpu.shape
-        organs_size_new = {}
+    def getInputImage(self, ori_inputs, ori_labels, ori_local_result, outputs_final_seg, mrf_i=0):
+        ## Get input image from original image
 
-        for i in range(len(organs_combine)):
-            organs_name = organs_combine_name[i]
-            organs_combine_list = organs_combine[organs_name]
-            organs_name = organs_name.split('-')[0]
-            for oc in organs_combine_list:
-                if 'outputs_one' not in dir():
-                    outputs_one = outputs_cpu[0, oc - 1, :, :, :]
+        net_inputs_list = []
+        crop_size_list = []
+
+        for oar_i in range(0, organs_num):
+
+            inputs = copy.deepcopy(ori_inputs)
+            labels = copy.deepcopy(ori_labels)
+            local_result = copy.deepcopy(ori_local_result)
+
+
+            if mrf_i == 0 or self.epoch < 10:
+                outputs_deform_new = copy.deepcopy(local_result)
+                if mrf_i == 0:
+                    outputs_mrf = torch.zeros_like(local_result)
+                    organs_size_flag = organs_size
                 else:
-                    outputs_one_a = outputs_cpu[0, oc - 1, :, :, :]
-                    outputs_one = outputs_one + outputs_one_a
+                    outputs_mrf = torch.from_numpy(outputs_final_seg.detach().cpu().numpy())
+                    organs_size_flag = organs_size
+            else:
+                outputs_deform_new = torch.from_numpy(outputs_final_seg.detach().cpu().numpy())
+                outputs_mrf = torch.from_numpy(outputs_final_seg.detach().cpu().numpy())
+                if mrf_i == 0:
+                    organs_size_flag = organs_size
+                else:
+                    organs_size_flag = organs_size
 
-            outputs_one = outputs_one / len(organs_combine_list)
-            outputs_one[outputs_one >= 0.5] = 1
-            outputs_one[outputs_one < 0.5] = 0
-            # outputs_one[outputs_one > 1] = 1
-            outputs_one = outputs_one.astype('int')
-            outputs_props = regionprops(outputs_one)
+            organ_name_LR, organ_name = self._getOrganName(oar_i)
+            crop_size_list.append(organs_size_flag[organ_name])
+            organ_id_combine = organs_combine[organ_name_LR]
 
-            if len(outputs_props) > 0:
-                outputs_props = outputs_props[0]
-                bbox = outputs_props['bbox']
-                center_point = outputs_props['centroid']
-                max_x = int(bbox[3] - center_point[0])
-                min_x = int(center_point[0] - bbox[0])
-                max_y = int(bbox[4] - center_point[1])
-                min_y = int(center_point[1] - bbox[1])
-                max_z = int(bbox[5] - center_point[2])
-                min_z = int(center_point[2] - bbox[2])
-                organ_size_new = [min_x, min_y, min_z, max_x, max_y, max_z]
-                organ_size_old = organs_size[organs_name]
-                for mi in range(0, 2):
-                    # if mi != 2:
-                    organ_size_new[mi] = np.minimum(organ_size_new[mi] + (organ_size_new[mi] % 2) + 6
-                                                    , organ_size_old[mi])
-                    organ_size_new[mi + 3] = np.minimum(organ_size_new[mi + 3] + (organ_size_new[mi + 3] % 2) + 6
-                                                        , organ_size_old[mi + 3])
-                mi = 2
-                organ_size_new[mi] = np.minimum(organ_size_new[mi] + (organ_size_new[mi] % 2) + 4
-                                                , organ_size_old[mi])
-                organ_size_new[mi + 3] = np.minimum(organ_size_new[mi + 3] + (organ_size_new[mi + 3] % 2 + 4)
-                                                    , organ_size_old[mi + 3])
-                    # else:
-                    #     organ_size_new[mi] = np.minimum(organ_size_new[mi] + (organ_size_new[mi] % 2)
-                    #                                     , organ_size_old[mi])
-                    #     organ_size_new[mi + 3] = np.minimum(organ_size_new[mi + 3] + (organ_size_new[mi + 3] % 2)
-                    #                                         , organ_size_old[mi + 3])
-            elif len(outputs_props) == 0:
-                organ_size_new = organs_size[organs_name]
-            organs_size_new.update({organs_name: organ_size_new})
-        del outputs_cpu, outputs_one, outputs_one_a, organ_size_old, organ_size_new, outputs, outputs_props
-        return organs_size_new
+            ## get location map
+            for oic in range(len(organ_id_combine)):
+                organ_id = organ_id_combine[oic]
+                if oic == 0:
+                    output_scores_map = outputs_deform_new[:, organ_id - 1:organ_id, :, :, :]
+                    outputs_mrf_map = outputs_mrf[:, organ_id - 1:organ_id, :, :, :]
+                    labels_flag = labels[:, organ_id - 1:organ_id, :, :, :]
+                else:
+                    output_scores_map = output_scores_map + outputs_deform_new[:, organ_id - 1:organ_id, :,
+                                                            :, :]
+                    outputs_mrf_map = outputs_mrf_map + outputs_mrf[:, organ_id - 1:organ_id, :, :, :]
+                    labels_flag = labels_flag + labels[:, organ_id - 1:organ_id, :, :, :]
+
+            if torch.sum(labels_flag).item() == 0:
+                continue
+
+        return
+
+    def _getOrganName(self, oar_i):
+        ## get OAR name
+        organ_name_LR = organs_combine_name[oar_i]
+        if '-L' in organ_name_LR:
+            organ_name = organ_name_LR.split('-L')[0]
+        elif '-R' in organ_name_LR:
+            organ_name = organ_name_LR.split('-R')[0]
+        else:
+            organ_name = organ_name_LR
+        return organ_name_LR, organ_name
+
+    def _getCentroid(self, matrix):
+        matrix_sum = torch.sum(matrix)
+
+        matrix_index = torch.ones([matrix.shape[2], matrix.shape[3], matrix.shape[4], 3])
+        matrix_index = matrix_index.cuda()
+
+        matrix_index[:, :, :, 0] = torch.mul(matrix_index[:, :, :, 0],
+                                             torch.reshape(torch.range(0, matrix.shape[2] - 1),
+                                                           [matrix.shape[2], 1, 1]))
+        matrix_index[:, :, :, 1] = torch.mul(matrix_index[:, :, :, 1],
+                                             torch.reshape(torch.range(0, matrix.shape[3] - 1),
+                                                           [1, matrix.shape[3], 1]))
+        matrix_index[:, :, :, 2] = torch.mul(matrix_index[:, :, :, 2],
+                                             torch.reshape(torch.range(0, matrix.shape[4] - 1),
+                                                           [1, 1, matrix.shape[4]]))
+
+        matrix_index_sum = torch.zeros([3])
+        matrix_index_sum[0] = torch.sum(torch.mul(matrix_index[:, :, :, 0], matrix))
+        matrix_index_sum[1] = torch.sum(torch.mul(matrix_index[:, :, :, 1], matrix))
+        matrix_index_sum[2] = torch.sum(torch.mul(matrix_index[:, :, :, 2], matrix))
+
+        center = (matrix_index_sum / matrix_sum)
+
+        return center.int()
+
+    def _getCropImage(self, matrix, center_point, crop_size):
+        n_batch, channels, height, width, depth = matrix.shape
+
+        centroid_z = center_point[2].item()
+        centroid_y = center_point[1].item()
+        centroid_x = center_point[0].item()
+
+        if self.mode == "train":
+            crop_size_i = crop_size[5] + crop_size[2]
+            random_i = random.randint(int(-0.1 * crop_size_i), int(0.1 * crop_size_i))
+            centroid_z = centroid_z + random_i
+            crop_size_i = crop_size[4] + crop_size[1]
+            random_i = random.randint(int(-0.1 * crop_size_i), int(0.1 * crop_size_i))
+            centroid_y = centroid_y + random_i
+            crop_size_i = crop_size[3] + crop_size[0]
+            random_i = random.randint(int(-0.1 * crop_size_i), int(0.1 * crop_size_i))
+            centroid_x = centroid_x + random_i
+
+        z2 = np.minimum(depth, centroid_z + crop_size[5])# + 3)
+        y2 = np.minimum(width, centroid_y + crop_size[4])# + 6)
+        x2 = np.minimum(height, centroid_x + crop_size[3])# + 6)
+
+        z1 = np.maximum(0, centroid_z - crop_size[2])# - 3)
+        y1 = np.maximum(0, centroid_y - crop_size[1])# - 6)
+        x1 = np.maximum(0, centroid_x - crop_size[0])# - 6)
+
+        mask_bbox = [z1,y1,x1,z2,y2,x2]
+
+        return mask_bbox
